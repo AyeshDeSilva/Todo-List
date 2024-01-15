88bd2a872a386a71536e602b51a755c801d3c354
Ayesh De Silva <adesi148@gmail.com>
2024-01-15 15:40:43 -0800

    Complete Todo List

---
+++ index.js
@@ -0,0 +1,58 @@
+import express  from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+let items = [];
+let workItems = [];
+
+app.get("/", (req, res) => {
+
+    let date = new Date();
+
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+    };
+
+    let today = date.toLocaleDateString('en-US', options);
+
+
+    res.render("index.ejs", {listTitle: today, newListItems: items} );
+
+});
+
+
+app.post("/", (req, res) => {
+
+    let item = req.body["newItem"];
+
+    if(req.body["list"] === "Work" ){
+
+        workItems.push(item);
+        res.redirect("/work");
+
+    }else{
+
+        items.push(item);
+        res.redirect("/");
+    }
+
+});
+
+app.get("/work", (req, res) => {
+
+
+    res.render("index.ejs", {listTitle: "Work List", newListItems: workItems})
+});
+
+
+app.listen(port, () => {
+    console.log("Listening to port " + port);
+})
